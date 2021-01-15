9f9d495cb44ba73e2e7d10b858aa5d0a1b1153c6
mark429 <mark429@gmail.com>
2021-01-15 14:40:33 -0500

    Update Configuration_adv.h
    changed sd abort behavior

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1182,7 +1182,7 @@
 
   //#define MENU_ADDAUTOSTART               // Add a menu option to run auto#.g files
 
-  #define EVENT_GCODE_SD_ABORT "G28XY"      // G-code to run on SD Abort Print (e.g., "G28XY" or "G27")
+  #define EVENT_GCODE_SD_ABORT "G1 Z3\nM84"      // G-code to run on SD Abort Print (e.g., "G28XY" or "G27")
 
   #if ENABLED(PRINTER_EVENT_LEDS)
     #define PE_LEDS_COMPLETED_TIME  (30*60) // (seconds) Time to keep the LED "done" color before restoring normal illumination
